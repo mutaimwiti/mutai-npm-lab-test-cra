e753f2e7a1f07790f7869160b0a245c55b1617dc
Mutai Mwiti <mutaimwiti40@gmail.com>
2022-11-11 17:59:30 +0300

    Test that exports are coming in correctly

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import * as Car from 'mutai-npm-lab/Car';
+import * as Foo from 'mutai-npm-lab/Foo';
+import * as Person from 'mutai-npm-lab/Person';
+
 import './App.css';
+
+const npmLab = require('mutai-npm-lab');
+
+console.log('root exports', Object.keys(npmLab));
+console.log('Car exports', Car);
+console.log('Foo exports', Foo);
+console.log('Person exports', Person);
+
 
 function App() {
   return (
